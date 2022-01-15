7ed1b22581294ba2b46a6b571b23624927a96fea
Chandra Kiran G <chandug1107@gmail.com>
2022-01-16 04:54:06 +0530

    querying  rendering data from database

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projects = Project.objects.all()
+    context = {'projects' : projects}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = Project.objects.get(id = pk)
+    context = {'project' : projectObj}
+    return render(request, 'projects/single-project.html', context)
