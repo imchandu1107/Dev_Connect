ec5371778e5d851ead3ca4e3b1b475a70a08d86b
Chandra Kiran G <chandug1107@gmail.com>
2022-01-24 05:02:06 +0530

    add and render user profiles

--- users/models.py
+++ users/models.py
@@ -12,6 +12,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.CharField(max_length=200, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
     headline = models.CharField(max_length=300, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default="profiles/defaultuser.png")
@@ -25,3 +26,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name="profiles"),
+    path('user-profile/<str:pk>', views.userProfile, name="user-profile"),
 ]
