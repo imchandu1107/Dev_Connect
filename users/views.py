61df989b65d450fcacf5ad77e123a103c0ba52f3
Chandra Kiran G <chandug1107@gmail.com>
2022-01-29 04:26:07 +0530

    User Actions: Updating Profile

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 # Create your views here.
 
@@ -52,7 +52,7 @@ def registerUser(request):
             user.save()
             messages.success(request, 'User account created successfully!')
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
 
         else:
             messages.error(request, 'An error occured while registration.')
@@ -82,3 +82,17 @@ def userAccount(request):
     projects = profile.project_set.all()
     context = {'profile':profile, 'skills':skills, 'projects':projects}
     return render(request, 'users/account.html', context)
+
+
+@login_required(login_url="login")
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+
+    context = {'form':form}
+    return render(request, 'users/profile_form.html', context)
